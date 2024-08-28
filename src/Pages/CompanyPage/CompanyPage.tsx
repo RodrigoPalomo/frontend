e9c7ad5b1157f6c8766db910a4d8f0342dee8f11
Rodrigo Palomo <repmlm@hotmail.com>
2024-08-28 13:50:58 -0300

    Agregué componentes y funcionalidades

--- src/Pages/CompanyPage/CompanyPage.tsx
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -4,6 +4,7 @@ import { CompanyProfile } from '../../company';
 import { getCompanyProfile } from '../../api';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import CompanyDashboard from '../../Components/CompanyDashboard/CompanyDashboard';
+import Tile from '../../Components/Tile/Tile';
 
 interface Props { }
 
@@ -24,7 +25,9 @@ const CompanyPage = (props: Props) => {
             {company ? (
                 <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
                     <Sidebar /> 
-                    <CompanyDashboard />
+                    <CompanyDashboard>
+                        <Tile title="Company Name" subTitle={company.companyName} />
+                    </CompanyDashboard>
                 </div>
             ) : (
                 <div>Company Not Found</div>
