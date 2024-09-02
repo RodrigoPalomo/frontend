9fbbab1f4a55de9c2c70ed59a7f52c56db6349f3
Rodrigo Palomo <repmlm@hotmail.com>
2024-09-02 16:07:44 -0300

    Funcionalidad de cashflow

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -8,6 +8,7 @@ import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 // import DesignPage from "../Pages/DesignPage/DesignPage";
 import DesignGuide from "../Pages/DesignGuide/DesignGuide";
 import BalanceSheet from "../Components/BalanceSheet/BalanceSheet";
+import CashflowStatement from "../Components/CashflowStatement/CashflowStatement";
 
 export const router = createBrowserRouter([
     {
@@ -24,6 +25,7 @@ export const router = createBrowserRouter([
                     { path: "company-profile", element: <CompanyProfile /> },
                     { path: "income-statement", element: <IncomeStatement /> },
                     { path: "balance-sheet", element: <BalanceSheet /> },
+                    { path: "cashflow-statement", element: <CashflowStatement /> },
                 ]
             },
         ]
